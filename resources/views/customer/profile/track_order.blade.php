ef95e5174ba12d45a1d69e9d5b2e8925230cf1c0
Vicky Chhetri <specialpower321@gmail.com>
2023-06-11 01:22:17 +0530

    order detail link

--- resources/views/customer/profile/track_order.blade.php
+++ resources/views/customer/profile/track_order.blade.php
@@ -13,7 +13,7 @@
                     <x-customer.profile-sidebar />
                     <div class="col-lg-8  col-md-6 col-12">
                         <div class="overview-wrapper">
-                            <h5 class="btn-light fs-5 py-2 ">Overview</h5>
+                            <h5 class="btn-light fs-5 py-2 ">Order Detail</h5>
                             <div class="d-flex gap-4 mt-5">
 
 
