10e79e30dc055777b8f49d9d0a90fce1a2012b46
Vicky Chhetri <specialpower321@gmail.com>
2023-06-11 01:21:42 +0530

    order detail link

--- resources/views/customer/profile/track_order.blade.php
+++ resources/views/customer/profile/track_order.blade.php
@@ -28,14 +28,7 @@
                                                     <span>   Order : {{$order->order_number}}</span>
 
                                                 </h4>
-                                                <div class="shadow-sm p-3">
-                                                    <p> Payment Status : <span class="{{$order->payment_status=='paid'?'text-success':'text-warning'}} p-1">
-                                    {{ucfirst($order->payment_status)}} </span>   </p>
-                                                    <p> Order Status : <span class="p-1">
-                                    {{strtoupper($order->delivery_status)}} </span>   </p>
 
-                                                    <p>  <b> Order Note: </b>  {{$order->order_notes??'Order notes not available.'}}</p>
-                                                </div>
                                             </div>
                                             <div class="card-body">
 
