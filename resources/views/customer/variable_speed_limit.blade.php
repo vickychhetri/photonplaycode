8a33e04f07cfda1e12eda4b9775ea01beb2f9234
Vicky Chhetri <specialpower321@gmail.com>
2023-06-11 20:32:54 +0530

    menu for mnobile

--- resources/views/customer/variable_speed_limit.blade.php
+++ resources/views/customer/variable_speed_limit.blade.php
@@ -62,13 +62,8 @@ $seo_meta=[
 
                                     @endforelse
 
-                                        <div id="modalOverlay" class="modal-overlay" onclick="hideModal()">
-                                            <div id="modalContent" class="modal-content">
-                                                <span class="closeImage text-danger "  onclick="hideModal()"> X</span> <!-- Add the close symbol here -->
+                                    <x-Customer.ShowImageDisplay/>
 
-                                                <img id="myImage" src="{{asset('assets/customer/images/zoom-in.png')}}" alt="Image" class="image-size">
-                                            </div>
-                                        </div>
                                 </div>
                             </div>
                         </div>
