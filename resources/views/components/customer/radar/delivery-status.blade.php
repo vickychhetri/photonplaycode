b22d0b99ae2b4ab6946c1f1d3dff1535893d738e
Vicky Chhetri <specialpower321@gmail.com>
2023-06-11 14:03:31 +0530

    menu for mnobile

--- resources/views/components/customer/radar/delivery-status.blade.php
+++ resources/views/components/customer/radar/delivery-status.blade.php
@@ -1,8 +1,9 @@
 
+@if((!isset($status)) || $status=='order')
 <div class="row justify-content-between">
     <div class="order-tracking completed">
         <span class="is-complete"></span>
-        <p>Ordered {{$status}} </p>
+        <p>Ordered  </p>
     </div>
     <div class="order-tracking  ">
         <span class="is-complete"></span>
@@ -13,3 +14,39 @@
         <p>Delivered</p>
     </div>
 </div>
+@elseif( $status=='shipped')
+    <div class="row justify-content-between">
+        <div class="order-tracking completed">
+            <span class="is-complete"></span>
+            <p>Ordered  </p>
+        </div>
+        <div class="order-tracking completed ">
+            <span class="is-complete"></span>
+            <p>Shipped </p>
+        </div>
+        <div class="order-tracking ">
+            <span class="is-complete"></span>
+            <p>Delivered</p>
+        </div>
+    </div>
+@elseif( $status=='delivered')
+
+    <div class="row justify-content-between">
+        <div class="order-tracking completed">
+            <span class="is-complete"></span>
+            <p>Ordered  </p>
+        </div>
+        <div class="order-tracking completed ">
+            <span class="is-complete"></span>
+            <p>Shipped </p>
+        </div>
+        <div class="order-tracking ">
+            <span class="is-complete"></span>
+            <p>Delivered</p>
+        </div>
+    </div>
+@else
+<div>
+    Status:: {{strtoupper($status)}}
+</div>
+@endif
