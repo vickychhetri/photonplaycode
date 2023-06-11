d1ece131c292064231d6df5ab7997601b820d4f6
Vicky Chhetri <specialpower321@gmail.com>
2023-06-11 22:53:06 +0530

    "Photonplay's Portable Variable Message Signs: Enhancing Traffic Management with Dynamic Messaging Solutions

--- resources/views/customer/portable_variable_message_signs.blade.php
+++ resources/views/customer/portable_variable_message_signs.blade.php
@@ -60,9 +60,9 @@ $seo_meta=[
             <div class="row">
                 <div class="col-lg-12 d-flex justify-content-center">
                     <div class="radar-icop w-75 ">
-                        <h2 class="mb-2">
+                        <h5 class="mb-2">
                             "Photonplay's Portable Variable Message Signs: Enhancing Traffic Management with Dynamic Messaging Solutions"
-                        </h2>
+                        </h5>
                         <p class="mt-4 mb-lg-0 mb-5 text-center">
                             Photonplay's Portable Variable Message Signs (VMS) are one of the key elements of dynamic
                             traffic management systems. Depending on the traffic situation, signs are efficiently used
