a6ad97ccaccc17e11c9f7a811d5179a042280ddd
satinder pal singh <satinderpal.henceforth@gmail.com>
2023-06-12 16:32:08 +0530

    Signed-off-by: satinder pal singh <satinderpal.henceforth@gmail.com>

--- resources/views/customer/cart/shopping_bag.blade.php
+++ resources/views/customer/cart/shopping_bag.blade.php
@@ -45,7 +45,8 @@
                                         <span>Brand : {{$cart->title}}</span>
                                         {{-- <span>Model: ---</span> --}}
                                         <span>Color : {{$cart->color}}</span>
-                                        @if (unserialize($cart->option_ids) != null)
+                                        @if (unserialize($cart->option_ids) == null)
+                                        @else
                                             @forelse (unserialize($cart->option_ids) as $option)
 
                                                 @php
