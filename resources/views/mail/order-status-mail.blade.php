fce9722de615a6e798cd4675647295cbea557076
Vicky Chhetri <specialpower321@gmail.com>
2023-06-12 18:45:57 +0530

    order status mail

--- resources/views/mail/order-status-mail.blade.php
+++ resources/views/mail/order-status-mail.blade.php
@@ -5,8 +5,90 @@
 <head>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{asset('assets/customer/css/style.css')}}">
+    <style>
+        table {
+            width: 100%;
+            /* Other table styles */
+        }
+
+        .hh-grayBox {
+            background-color: #F8F8F8;
+            margin-bottom: 20px;
+            padding: 35px;
+            margin-top: 20px;
+        }
+        .pt45{padding-top:45px;}
+        .order-tracking{
+            text-align: center;
+            width: 33.33%;
+            position: relative;
+            display: block;
+        }
+        .order-tracking .is-complete{
+            display: block;
+            position: relative;
+            border-radius: 50%;
+            height: 30px;
+            width: 30px;
+            border: 0px solid #AFAFAF;
+            background-color: #f7be16;
+            margin: 0 auto;
+            transition: background 0.25s linear;
+            -webkit-transition: background 0.25s linear;
+            z-index: 2;
+        }
+        .order-tracking .is-complete:after {
+            display: block;
+            position: absolute;
+            content: '';
+            height: 14px;
+            width: 7px;
+            top: -2px;
+            bottom: 0;
+            left: 5px;
+            margin: auto 0;
+            border: 0px solid #AFAFAF;
+            border-width: 0px 2px 2px 0;
+            transform: rotate(45deg);
+            opacity: 0;
+        }
+        .order-tracking.completed .is-complete{
+            border-color: #27aa80;
+            border-width: 0px;
+            background-color: #27aa80;
+        }
+        .order-tracking.completed .is-complete:after {
+            border-color: #fff;
+            border-width: 0px 3px 3px 0;
+            width: 7px;
+            left: 11px;
+            opacity: 1;
+        }
+        .order-tracking p {
+            color: #A4A4A4;
+            font-size: 16px;
+            margin-top: 8px;
+            margin-bottom: 0;
+            line-height: 20px;
+        }
+        .order-tracking p span{font-size: 14px;}
+        .order-tracking.completed p{color: #000;}
+        .order-tracking::before {
+            content: '';
+            display: block;
+            height: 3px;
+            width: calc(100% - 40px);
+            background-color: #f7be16;
+            top: 13px;
+            position: absolute;
+            left: calc(-50% + 20px);
+            z-index: 0;
+        }
+        .order-tracking:first-child:before{display: none;}
+        .order-tracking.completed:before{background-color: #27aa80;}
+
+
+    </style>
 </head>
 <body>
 
