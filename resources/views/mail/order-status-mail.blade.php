63769649731eb185405625a21358cdb3888cf4d7
Vicky Chhetri <specialpower321@gmail.com>
2023-06-12 20:42:45 +0530

    order status mail

--- resources/views/mail/order-status-mail.blade.php
+++ resources/views/mail/order-status-mail.blade.php
@@ -4,7 +4,9 @@
 <html>
 <head>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Add this in the <head> section of your email template -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
         table {
             width: 100%;
