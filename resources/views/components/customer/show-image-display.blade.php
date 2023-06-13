099cb329e6fbbed8663c87fcbadb0ea41831430f
Vicky Chhetri <specialpower321@gmail.com>
2023-06-14 02:38:49 +0530

    index

--- resources/views/components/customer/show-image-display.blade.php
+++ resources/views/components/customer/show-image-display.blade.php
@@ -1,5 +1,5 @@
-<div id="modalOverlay" class="modal-overlay bg-white" onclick="hideModal()">
-    <div id="modalContent" class="modal-content">
+<div id="modalOverlay" class="modal-overlay bg-white p-3" onclick="hideModal()">
+    <div id="modalContent" class="modal-content ">
         <span class="closeImage text-danger "  onclick="hideModal()"> X</span> <!-- Add the close symbol here -->
 
         <img id="myImage" src="{{asset('assets/customer/images/zoom-in.png')}}" alt="Image" class="image-size">
