6f0ec3190b5abcd7a6b1045b1d96d104f32876f5
Vicky Chhetri <specialpower321@gmail.com>
2023-06-15 15:02:25 +0530

    pvms image

--- resources/views/components/customer/show-image-display.blade.php
+++ resources/views/components/customer/show-image-display.blade.php
@@ -1,6 +1,7 @@
-<div id="modalOverlay" class="modal-overlay p-3" onclick="hideModal()">
-    <div id="modalContent" class="modal-content ">
-        <span class="closeImage text-danger "  onclick="hideModal()"> X</span> <!-- Add the close symbol here -->
+<div id="modalOverlay" class="modal-overlay p-3"  onclick="hideModal()">
+    <span class="closeImage text-danger " style="position: absolute;top: 0;right: 20px;" onclick="hideModal()"> X</span> <!-- Add the close symbol here -->
+    <div id="modalContent" class="modal-content position-relative" >
+
 
         <img id="myImage" src="{{asset('assets/customer/images/zoom-in.png')}}" alt="Image" class="image-size">
     </div>
