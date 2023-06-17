0b476fe823702c9c0eaf1d93095239734aa40348
Vicky Chhetri <specialpower321@gmail.com>
2023-06-17 10:14:02 +0530

    vms

--- resources/views/customer/pvms_icop.blade.php
+++ resources/views/customer/pvms_icop.blade.php
@@ -48,24 +48,7 @@ $seo_meta=[
                 @endfor
 
             </div>
-{{--            <button class="carousel-control-prev" data-bs-target="#carouselExampleDark" type="button"--}}
-{{--                data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">--}}
-{{--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-{{--                <span class="visually-hidden">Previous</span>--}}
-{{--            </button>--}}
-{{--            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"--}}
-{{--                data-bs-target="#carouselExampleIndicators" data-bs-slide="next">--}}
-{{--                <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-{{--                <span class="visually-hidden">Next</span>--}}
-{{--            </button>--}}
-            <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button> -->
+
         </div>
     </section>
 
