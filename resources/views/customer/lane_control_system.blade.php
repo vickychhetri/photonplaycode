42ab14a225e5ef55fd453f4293a17566eaf7b07b
Vicky Chhetri <specialpower321@gmail.com>
2023-06-17 20:46:12 +0530

    LCS

--- resources/views/customer/lane_control_system.blade.php
+++ resources/views/customer/lane_control_system.blade.php
@@ -17,7 +17,7 @@ $seo_meta=[
                 <div class="imaged m-auto">
                     <div class="city-wrap">
                         <h2 class=" text-white fw-normal mb-1 title-text-h2">{{$page->title}}</h2>
-                        <p class=" text-white fw-normal mt-2 mb-2 ">Highly visible and innovative, creating instant awareness of <br/>local speed limit</p>
+                        <p class=" text-white fw-normal mt-2 mb-2 ">Offering real-time monitoring and control of traffic flow</p>
                         <a  href="#inquiry" class="btn-primary-rounded p-0 m-0 d-flex align-items-center justify-content-center get-quote-button-header-model" >GET QUOTE</a>
                     </div>
                     <div  class="desktop-display">
