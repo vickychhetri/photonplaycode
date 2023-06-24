6d139365e73fcf7851c730acb76099c9bbd1cc2f
Vicky Chhetri <specialpower321@gmail.com>
2023-06-25 00:05:37 +0530

    smart city

--- resources/views/customer/vms_sub_page.blade.php
+++ resources/views/customer/vms_sub_page.blade.php
@@ -20,7 +20,7 @@ $seo_meta=[
                         <a  href="#inquiry" class="btn-primary-rounded p-0 m-0 d-flex align-items-center justify-content-center get-quote-button-header-model" >GET QUOTE</a>
                     </div>
                     <div class="desktop-display">
-                        <img src="{{asset('storage/'.$page->cover_image)}}"  alt="alt" class="d-block img-fluid h-75 product-feature-model-image"  style="transform: scale(1.5);">
+                        <img src="{{asset('storage/'.$page->cover_image)}}"  alt="alt" class="d-block img-fluid h-75 product-feature-model-image"  style="transform: scale(1.8);">
                     </div>
 
                 </div>
