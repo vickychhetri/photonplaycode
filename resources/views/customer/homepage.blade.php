25e0d8cc8692bfde13a06bf2f61826cfa8bb7e14
Vicky Chhetri <specialpower321@gmail.com>
2023-07-06 01:05:20 +0530

    Manage Seo migration

--- resources/views/customer/homepage.blade.php
+++ resources/views/customer/homepage.blade.php
@@ -1,8 +1,11 @@
 <?php
+use App\Models\ManageSeo;
+$data_record = ManageSeo::where('page_name',ManageSeo::HOME)->first();
 $seo_meta=[
-    "title"=>"Variable Message Signs | Radar Speed Signs | Driver Feedback Signs - Photonplay",
-    "description"=>"PhotonPlay is a financially independent, global technology company established in 2006 with wholly-owned subsidiaries in the USA, Norway, and Australia.",
-    "keywords"=>"photonplay, radar speed sign, variable message signs, driver feedback"
+    "title"=>$data_record->title,
+    "description"=>$data_record->description,
+    "keywords"=>$data_record->keyword,
+    "schema"=>$data_record->schema
 ];
 ?>
 @include('customer.layout2.header')
