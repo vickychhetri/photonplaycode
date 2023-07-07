5872c4e0fa8c63b83fe3c78796d21f83f558079d
Vicky Chhetri <specialpower321@gmail.com>
2023-07-07 14:44:42 +0530

    google site verification

--- routes/customer/api2.php
+++ routes/customer/api2.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\ThankYouController;
 Route::get('blog/{page_name}', [ContactUsController::class, 'blog_show'])->name('blog_show');
 Route::get('blog/{blog_id}/like-unlike', [BlogLikeController::class, 'like_unlike'])->name('blog_like_unlike');
 Route::get('blog', [ContactUsController::class, 'blog_listing'])->name('blog');
+Route::get('blogs', [ContactUsController::class, 'blog_listing'])->name('blog');
 Route::get('/content/search', [SearchItemsController::class, 'search_index'])->name('search_photon_things');
 
 Route::get('/photon/{page_name}', [CustomerProfileController::class, 'page_show'])->name('page_show_content');
