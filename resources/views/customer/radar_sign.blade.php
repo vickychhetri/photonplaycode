9b22f18ce606cf7bb6dde1abbb18c2c9984ef4bc
Vicky Chhetri <specialpower321@gmail.com>
2023-07-07 09:53:45 +0530

    remove ``

--- resources/views/customer/radar_sign.blade.php
+++ resources/views/customer/radar_sign.blade.php
@@ -356,6 +356,8 @@ if(isset($seo_record)){
         </div>
     </div>
 </section>
+
+<h1> ye price {{$product->price}}</h1>
 @include('customer.layout2.footer')
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.js"></script>
