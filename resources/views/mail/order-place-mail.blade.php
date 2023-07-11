86a24e77414080419e16bc5a27d82e888cf53864
Vicky Chhetri <specialpower321@gmail.com>
2023-07-11 22:58:52 +0530

    data

--- resources/views/mail/order-place-mail.blade.php
+++ resources/views/mail/order-place-mail.blade.php
@@ -3,23 +3,14 @@
 
 Dear Customer,
 We would like to express our sincere gratitude for choosing to purchase our product. This email serves as both a thank you and an acknowledgment of your recent purchase. On behalf of Photonplay System, we want to assure you that your satisfaction is our top priority, and we are committed to delivering a seamless and exceptional experience throughout your journey with us. We are pleased to confirm that your order has been successfully received and is currently being processed. Our dedicated team is working diligently to prepare your package for shipping.
-
-<table>
-    <tr>
-        <td>
-            Order Number
-        </td>
-        <th>
-            {{$data["order_number"]}}
-        </th>
-    </tr>
+<table width="100%">
+    <tr><td>Order Number</td><th>{{$data["order_number"]}}</th></tr>
     <tr>
         <td>
             Product
         </td>
         <th>
             {{$data["product_name"]}}
-
         </th>
     </tr>
     <tr>
@@ -70,9 +61,7 @@ We would like to express our sincere gratitude for choosing to purchase our prod
             {{$data["delivery_status"]}}
         </th>
     </tr>
-
 </table>
-
 
 Thanks,<br>
 {{ config('app.name') }}
