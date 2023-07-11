a68a5907912f586b465bdf97e1730b5fd6838e93
Vicky Chhetri <specialpower321@gmail.com>
2023-07-12 00:46:30 +0530

    mail

--- resources/views/mail/order-place-mail.blade.php
+++ resources/views/mail/order-place-mail.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 Dear Customer,<br/>
-We would like to express our sincere gratitude for choosing to purchase our product. This email serves as both a thank you and an acknowledgment of your recent purchase. On behalf of Photonplay System, we want to assure you that your satisfaction is our top priority, and we are committed to delivering a seamless and exceptional experience throughout your journey with us. We are pleased to confirm that your order has been successfully received and is currently being processed. Our dedicated team is working diligently to prepare your package for shipping.
+We would like to express our sincere gratitude for choosing to purchase our product. This email serves as both a thank you and an acknowledgment of your recent purchase.
 <style>
     table, th, td {
         border: 1px solid black;
