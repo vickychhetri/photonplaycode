b86026a39bdc103cc9c9c32e324a96234fdd6124
Vicky Chhetri <specialpower321@gmail.com>
2023-07-15 17:57:44 +0530

    more gap

--- resources/views/customer/radar_sign.blade.php
+++ resources/views/customer/radar_sign.blade.php
@@ -340,7 +340,7 @@ if(isset($seo_record)){
                                         Radar Speed Sign
                                     </span>
                                     <span class="d-block">{{$more_product->title}}</span>
-                                    <div class="d-flex justify-content-center align-items-center my-2">
+                                    <div class="d-flex justify-content-center align-items-center my-2 gap-1">
                                         <img src="{{ asset('assets\customer\images\star.svg') }}" alt="Not Found" class="img-fluid"
                                              width="14px">
                                         <img src="{{ asset('assets\customer\images\star.svg') }}" alt="Not Found" class="img-fluid"
