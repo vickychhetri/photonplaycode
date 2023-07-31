d9fa072951550be6447314b859db336e73dda8a6
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-07-31 15:20:22 +0530

    Circular_outer_led_ticker.webp

--- resources/views/customer/homepage.blade.php
+++ resources/views/customer/homepage.blade.php
@@ -505,7 +505,7 @@ $seo_meta=[
                                     {{--                                <p class="mb-0">{{$product->title}}</p>--}}
                                 </div>
                                 <div class="mask-layer common-wdth inner-col text-center">
-                                    <img src="/assets/customer/images/products_home/Circular_outer_led_ticker.png" alt=" LED Tickers" class="">
+                                    <img src="/assets/customer/images/products_home/Circular_outer_led_ticker.webp" alt=" LED Tickers" class="">
                                     <div>
                                         <a href="{{route('customer.solution.city')}}" class="btn btn-primary text-capitalize  py-0 px-3 m-auto mt-3 mb-4"> Know More</a>
                                     </div>
