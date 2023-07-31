389988ee420381016a11b9310b3bec7df322b5ca
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-07-31 16:01:27 +0530

    cdn

--- resources/views/customer/layout2/header.blade.php
+++ resources/views/customer/layout2/header.blade.php
@@ -26,7 +26,7 @@
 
 
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 {{--    <link href="/assets/customer/css/bootstrap.min.css" rel="stylesheet">--}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
