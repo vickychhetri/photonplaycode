d04c09e2d9124eb264495f64cc0523238aa6d7a4
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-08-02 15:04:14 +0530

    reverse blog

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 class CommonController extends Controller
 {
     public function upload(Request $request)
     {
         $file = $request->file('photo');
-        $filename = uniqid() . '.' . $file->getClientOriginalExtension();
-        $file->storeAs('public/photos', $filename);
+        $filename='';
+//        $filename = uniqid() . '.' . $file->getClientOriginalExtension();
+//        $file->storeAs('public/photos', $filename);
+        if (isset($request->photo)) {
+            $originalName = $request->file('photo')->getClientOriginalName();
+            $extension = $request->file('photo')->getClientOriginalExtension();
+            $number = 0;
+            $imageName = $originalName;
+
+            while (Storage::disk('public')->exists('public/photos/' . $imageName)) {
+                $number++;
+                $imageName = pathinfo($originalName, PATHINFO_FILENAME) . '_' . $number . '.' . $extension;
+            }
+
+            $request->file('image')->storeAs('public/photos', $imageName, 'public');
+            $filename=$imageName;
+        }
+
         return response()->json([
             'url' => asset('storage/photos/' . $filename)
         ]);
+
     }
 
     public function optimize(){
