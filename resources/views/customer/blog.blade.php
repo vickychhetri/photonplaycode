a2d388a13381421307e8a481d2c5752af1aa7ccb
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-08-02 12:57:36 +0530

    reverse blog

--- resources/views/customer/blog.blade.php
+++ resources/views/customer/blog.blade.php
@@ -25,7 +25,7 @@ $seo_meta=[
                     <div class="post-item mb-5">
                         <img src="{{asset("storage/".$blog->image)}}" alt="" class="mb-4 img-fluid" style="max-height: 400px;">
                         <div class="pb-3 post-info border-0">
-                            <h1 class="text-uppercase mb-3 text-dark"> {{$blog->title}} </h1>
+                            <h1 class="text-uppercase mb-3 text-dark" style="font-size: 36px;"> {{$blog->title}} </h1>
                             <div class="mb-4">
                               {{$blog_created_date}} by {{$blog->author}}
 {{--                                @foreach($tags as $tag)--}}
