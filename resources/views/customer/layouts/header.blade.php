72a92f041a7e6e5b43c15b67eddb97073d8a3683
Vicky Chhetri <specialpower321@gmail.com>
2023-08-09 06:44:43 +0530

    find location

--- resources/views/customer/layouts/header.blade.php
+++ resources/views/customer/layouts/header.blade.php
@@ -321,7 +321,7 @@ $productLists = Product::take(5)->get();
 
                 @endif
 
-
+0
         </ul>
         </div>
     </nav>
@@ -359,5 +359,30 @@ $productLists = Product::take(5)->get();
 
 </script>
 
-    </header>
+
+    <script>
+        // Fetch user's IP information using ip-api API
+        fetch('http://ip-api.com/json')
+            .then(response => response.json())
+            .then(data => {
+                const ipAddress = data.query;
+                const city = data.city;
+                const region = data.regionName;
+                const country = data.country;
+
+                console.log('IP Address:', ipAddress);
+                console.log('City:', city);
+                console.log('Region:', region);
+                console.log('Country:', country);
+
+                // Now you can use this information as needed
+            })
+            .catch(error => {
+                console.error('Error fetching IP information:', error);
+                // You can handle errors here
+            });
+
+    </script>
+
+</header>
 
