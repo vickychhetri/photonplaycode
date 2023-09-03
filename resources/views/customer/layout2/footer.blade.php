b3135c720a4e52fe74be77a85e828621e0379d12
Vicky Chhetri <specialpower321@gmail.com>
2023-09-03 11:12:46 +0530

    add modal
     custome
    
    33

--- resources/views/customer/layout2/footer.blade.php
+++ resources/views/customer/layout2/footer.blade.php
@@ -35,27 +35,28 @@ $blogs = Blog::select('slug','title')->take(4)->get();
 
 <script>
     // Add a click event listener to the button
+    document.getElementById('startvideochat').addEventListener('click', function () {
+        //     console.log("hitted");
+        window.addEventListener('consoltoEvent', (e) => { // Listen to events sent from Consolto
 
-    window.addEventListener('consoltoEvent', (e) => { // Listen to events sent from Consolto
+            if (e.detail.consoltoReady) { // Will be true only when Consolto becomes ready
 
-        if (e.detail.consoltoReady) { // Will be true only when Consolto becomes ready
+                var event = new CustomEvent('consolto_h2w', { // Create a custom event that will be dispatched and trigger one of the actions.
 
-            var event = new CustomEvent('consolto_h2w', { // Create a custom event that will be dispatched and trigger one of the actions.
+                    detail: {
 
-                detail: {
+                        'et-click-type': 'start-call', // One of the 5 available actions
 
-                    'et-click-type': 'start-call', // One of the 5 available actions
+                    },
 
-                },
+                });
 
-            });
+                window.dispatchEvent(event); // Here we programmatically trigger the Consolto widget
 
-            window.dispatchEvent(event); // Here we programmatically trigger the Consolto widget
+            }
 
-        }
-
+        });
     });
-
 
     // document.getElementById('startvideochat').addEventListener('click', function () {
     //     console.log("hitted");
