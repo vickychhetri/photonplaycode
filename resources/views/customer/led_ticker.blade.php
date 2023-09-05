58a8908eaf3a2cddfd17588c07e4961054f96df4
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-09-05 10:16:31 +0530

    led and bus sign controller

--- resources/views/customer/led_ticker.blade.php
+++ resources/views/customer/led_ticker.blade.php
@@ -1,7 +1,7 @@
 <?php
 $seo_meta=[
-    "title"=>"iCop | RADAR SPEED SIGN - No.1 In Traffic Claiming Solution",
-    "description"=>" The sign acts like a Variable Message Sign to display graphics and text along with white Alert Flashing Lights . The sign acts as a Smart Traffic data collector and analyzer. It is a cloud controlled and highly secured Radar Speed Sign.",
+    "title"=>"Vehicle Actuated Speed Display",
+    "description"=>"Enhance road safety with our Vehicle Actuated Speed Displays (VASD). Instantly recognize vehicle speed for efficient traffic flow on roads, highways, and tunnels.",
     "keywords"=>"photonplay, radar speed sign, variable message signs, driver feedback"
 ];
 
@@ -11,7 +11,7 @@ $seo_meta=[
 {{--<x-Customer.Radar.RadarBanner/>--}}
 
 <section class="banner-inner pt-0 pb-0" >
-    <div id="carouselExampleDark" class="carousel slide banner-image-new-cloud-software-parent" data-bs-ride="carousel">
+    <div id="carouselExampleDark" class="carousel slide banner-image-new-vasd-parent" data-bs-ride="carousel">
         <div class="carousel-indicators">
         </div>
         <div class="carousel-inner">
@@ -22,12 +22,14 @@ $seo_meta=[
 
                         <div class="position-relative heading-banner  ">
 
-                            <h1 class="text-dark">RADAR CLOUD MANAGEMENT
-                                <p class="mb-0 h4 font-weight-bold"><span> Manage Your Speed Sign Anywhere, Anytime </span></p>
+                            <h1 class="text-dark" style="font-size: 36px;">Vehicle Actuated Speed Display
+                                <p class="mb-0 h4 font-weight-bold text-primary"><span> Traffic Management Tool For Real time speed feedback  and Enforcement </span></p>
                             </h1>
 
-                            <div class="zigzack d-flex justify-content-start"><img src="{{asset('assets\customer\images\ziczac.png')}}"
-                                                                                   class="img-fluid d-none d-md-block" alt="not-found"></div>
+
+
+                            {{--                            <div class="zigzack d-flex justify-content-start"><img src="{{asset('assets\customer\images\ziczac.png')}}"--}}
+                            {{--                                                                                   class="img-fluid d-none d-md-block" alt="not-found"></div>--}}
                             <div
                                 class="circle-dotted position-absolute w-100 d-none d-md-flex align-items-center justify-content-start">
                                 {{--                                    <img src="{{asset('assets\customer\images\circles.png')}}" alt="Not Found" class="img-fluid">--}}
@@ -35,6 +37,8 @@ $seo_meta=[
                         </div>
                         <div class="desktop-display">
 
+                            <img src="{{asset('assets\customer\images\vasd\camerab.webp')}}" alt="Not Found"
+                                 class="img-fluid d-none d-md-block">
                         </div>
                         <img src="{{asset('assets\customer\images\circlecolor.png')}}" alt="Not Found"
                              class="img-fluid d-none d-md-block">
@@ -54,9 +58,9 @@ $seo_meta=[
         <div class="row">
             <div class="col-lg-12 ">
                 <div class="radar-icop">
-                    <h4 class="text-uppercase text-center"> CONTROL RADAR SPPED SIGNS VIA CENTRALIZED RADAD CLOUD MANAGEMENT SOFTWARE.</h4>
+                    <h4 class="text-uppercase text-center">  Vehicle-Actuated Speed Display (VASD)  </h4>
                     <p class="mt-4 text-center">
-                        The complete sign management includes basic sign configuration and settings, alert management, data analysis and data collection from anywhere for the connected device without installing any application.
+                        Vehicle-Actuated Speed Display (VASD) is a traffic management tool used to display the speed of approaching vehicles to encourage them to adhere to the speed limit. The basic concept behind a VASD is to provide drivers with real-time feedback on their current speed. The device consists of a radar sensor that measures the speed of approaching vehicles. The measured speed is then displayed on a prominent electronic sign or LED display board When a vehicle exceeds the predefined threshold speed (usually the posted speed limit), the VASD activates the display, which shows the vehicle's current speed. The intention is to remind drivers of their speed and encourage them to slow down, promoting safer driving habits.  VASD ANPR systems play a crucial role in enforcing traffic laws. By automatically classifying the vehicle class,  reading license plates, ANPR cameras can identify vehicles that are speeding, or violating other traffic regulations. The system integrates with standard government portals for enforcing chalans .  ANPR technology enables law enforcement agencies to efficiently monitor traffic and enforce compliance with road safety regulations.
                     </p>
                 </div>
             </div>
@@ -69,146 +73,137 @@ $seo_meta=[
 <!-- iCop Series Features Start -->
 <section class="pt-0 mt-0" >
     <div class="container">
-        <div class="row gy-5" style="margin-right: 12%;margin-left: 7%;" >
+        <div class="row gy-5"   >
             <div class="col-lg-12">
                 <div class="text-center ">
-                    <h2 class="fs-md-2 mt-3">Cloud Features</h2>
+                    <h2 class="fs-md-2 mt-3"> Features</h2>
                 </div>
             </div>
 
-            <div class="col-lg-6  ">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center ">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s1.png')}}" style="height: 70px;"  alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v1.png')}}" style="height: 70px;"  alt="Not Found"></div>
                         <div class="">
-                            <h6>24/7 Uptime</h6>
-                            <p class="mb-0"> Enhance the operational capabilities</p>
+                            <h6>Vehicle classification  </h6>
+                            <p class="mb-0" style="font-size: 10px;"> categorizing different types of vehicles based on size like two wheerlers , four wheelers , heavy vehicles </p>
                         </div>
 
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c1.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/1.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s2.png')}}" style="height: 70px;" alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v2.png')}}" style="height: 70px;" alt="Not Found"></div>
                         <div class="">
-                            <h6>GPS Device Locator</h6>
-                            <p class="mb-0"> Live Map to view all the device locations</p>
+                            <h6>ANPR Solution</h6>
+                            <p class="mb-0" style="font-size: 10px;"> Automatic number plate recognition with high accuracy  </p>
 
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c2.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/2.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s3.png')}}" style="height: 70px;"  ></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v3.png')}}" style="height: 70px;"  ></div>
                         <div class="">
-                            <h6>User Authentication</h6>
-                            <p class="mb-0"> Provides multiple user level access </p>
+                            <h6>Traffic Enforcement </h6>
+                            <p class="mb-0" style="font-size: 10px;">  All the infractions are logged and integrate with central law enforcement agencies  like VAHAN portal  </p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c3.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/3.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s4.png')}}"  style="height: 70px;" alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v4.png')}}"  style="height: 70px;" alt="Not Found"></div>
                         <div class="">
-                            <h6>Schedules</h6>
-                            <p class="mb-0"> Schedules events by days, weeks and months dynamically</p>
+                            <h6>Automatic Lane Identification</h6>
+                            <p class="mb-0" style="font-size: 10px;"> Automatic and accurate detection of Lane in multi lane scenario </p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c4.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/4.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
 
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s5.png')}}" style="height: 70px;" alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v5.webp')}}" style="height: 70px;" alt="Not Found"></div>
                         <div class="">
-                            <h6>Centrally Controlled</h6>
-                            <p class="mb-0"> Single point of connect for multiple signs</p>
+                            <h6>24x7 Operation </h6>
+                            <p class="mb-0" style="font-size: 10px;"> Fully optimized to work in day and night conditions</p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c5.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/5.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center ">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s6.png')}}" style="height: 70px;"  alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v6.png')}}" style="height: 70px;"  alt="Not Found"></div>
                         <div class="">
-                            <h6>Alert Management</h6>
-                            <p class="mb-0"> Alerts for the events like over speeding and low battery </p>
+                            <h6>High performance  Radar</h6>
+                            <p class="mb-0" style="font-size: 10px;"> High performance 4D radar is used to detect the speeds and Lanes  </p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c6.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/6.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center ">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s7.png')}}" style="height: 70px;"  alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v7.png')}}" style="height: 70px;"  alt="Not Found"></div>
                         <div class="">
-                            <h6>Pro Data Analysis</h6>
-                            <p class="mb-0"> Data analysis and report generations</p>
+                            <h6>Clear Visibility </h6>
+                            <p class="mb-0" style="font-size: 10px;"> The speed signs are visible from a distance of 300mtrs . The motorist shall be well aware of speed from a distance </p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c7.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/7.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <div class="feature-card p-4 bg-white">
                     <div class="d-flex align-items-center ">
-                        <div class="me-3"><img src="{{asset('assets/customer/images/cloudmanager/icon/s8.png')}}" style="height: 70px;"  alt="Not Found"></div>
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v8.png')}}" style="height: 70px;"  alt="Not Found"></div>
                         <div class="">
-                            <h6>Cost Reduction</h6>
-                            <p class="mb-0"> Remote connectivity reduces the operational cost </p>
+                            <h6>Dynamic Message Broadcast</h6>
+                            <p class="mb-0" style="font-size: 10px;"> Overspeed warning messages and white flashing strobe lights for over speeding vehicles  </p>
                         </div>
                     </div>
-                    <img src="{{asset('assets/customer/images/cloudmanager/c8.png')}}"  class="img-fluid p-2"/>
+                    <img src="{{asset('assets/customer/images/vasd/features/8.jpg')}}"  class="img-fluid p-2"/>
                 </div>
             </div>
 
 
+            <div class="col-lg-4">
+                <div class="feature-card p-4 bg-white">
+                    <div class="d-flex align-items-center ">
+                        <div class="me-3"><img src="{{asset('assets/customer/images/vasd/icon/v1.png')}}" style="height: 70px;"  alt="Not Found"></div>
+                        <div class="">
+                            <h6>Highly Accurate</h6>
+                            <p class="mb-0" style="font-size: 10px;"> The system is highly accurate to achieve an accuracy of more than 95% on vehicle detection , classification and ANPR </p>
+                        </div>
+                    </div>
+                    <img src="{{asset('assets/customer/images/vasd/features/9.jpg')}}"  class="img-fluid p-2"/>
+                </div>
+            </div>
 
         </div>
     </div>
 </section>
 <!-- iCop Series Features End -->
 
-{{--hIDE GALLRY--}}
-{{--<div class="container-fluid mb-0" style="overflow:hidden;">--}}
-{{--    <div class="col-lg-12">--}}
-{{--        <div class="text-center">--}}
-{{--            <h2 class="fs-md-2 mt-3">Gallery</h2>--}}
-{{--            <p class="fs-6">Our Key Projects Across the Globe - Discover How Our Innovative Solutions are Changing the Game!</p>--}}
-{{--        </div>`--}}
-{{--    </div>--}}
-{{--    <div class="col-lg-12 pb-4 pt-0 mt-0">--}}
-{{--        <div class="key-slider-sign-radar slide-images-key">--}}
-{{--            <div>--}}
-{{--                <img src="/assets/images/radar/gallery/UV9D2BRMlg5xvAE4fjxZdmNw1Kv4i731VzOTKlHN.jpg"  class="img-fluid w-100 h-100 slide-images-key">--}}
-{{--            </div>--}}
-{{--            <div>--}}
-{{--                <img src="/assets/images/radar/gallery/0W2kZs8THRJN0mSOI6Lr9J4OxCLJ4gx4BjGwG5jn.jpg"  class="img-fluid w-100 h-100 slide-images-key">--}}
-{{--            </div>--}}
-
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-{{--hIDE GALLERY END--}}
 <!--  -->
 @include('customer.layout2.get_in_touch')
 <!-- _____________________ourclint-last-start___________________ -->
