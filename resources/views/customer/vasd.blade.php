48d3de7aaeabcdf55b548a81d342269cf337258f
Vicky Chhetri <specialpower321@gmail.com>
2023-09-12 03:50:49 +0530

    VSAD FAQ

--- resources/views/customer/vasd.blade.php
+++ resources/views/customer/vasd.blade.php
@@ -211,6 +211,151 @@ $seo_meta=[
 
 <!--  -->
 @include('customer.layout2.get_in_touch')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="fs-md-2 mt-3 text-center">  FAQ  </h2>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            1.	What are vehicle-actuated speed signs?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Vehicle-actuated speed signs are electronic road signs that track a car's speed and show it to the driver in real time. These signs encourage slower driving behavior and increase road safety by giving drivers immediate feedback about their current speed.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            2.	How do Vehicle-Actuated Speed Displays (VASD) work?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Sensors are used by VASDs to determine an approaching vehicle's speed. Based on the vehicle's distance from the sign and the time needed to travel that distance, the displayed speed is calculated. This real-time data is then shown on the display board for the driver to see.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            3.	Are VASDs connected to a central system?
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Some VASDs are linked to a central control system that keeps track of traffic and can remotely change the speed limits shown based on that traffic. Others operate independently with predefined speed limits.
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingFour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            4.	Do VASDs have any night-time visibility features?
+                        </button>
+                    </h2>
+                    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Yes, a lot of VASDs have reflective features or lighting to help them be seen at night or in low-light conditions.
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingFive">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            5.	Where are VASDs commonly used?
+                        </button>
+                    </h2>
+                    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: VASDs are commonly used on highways, roads with high accident rates, school zones, construction zones, and areas with speed limit changes. They are also used in smart cities to encourage safer driving.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingSix">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                            6.	How effective are VASDs in reducing speeding?
+                        </button>
+                    </h2>
+                    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Studies have shown that VASDs can reduce speeding by up to 15%. These tools give drivers quick feedback and serve as a reminder to follow the speed restrictions.
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingSeven">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                            7.	Can VASDs really prevent accidents?
+                        </button>
+                    </h2>
+                    <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Yes, VASDs help prevent accidents by urging cars to slow down. Reduced speeds result in shorter stopping distances, giving drivers more time to respond to unforeseen circumstances and reducing the likelihood of collisions.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingEightA">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEightA" aria-expanded="false" aria-controls="collapseEightAx`blue ">
+                            8.	Are VASDs a global phenomenon?
+                        </button>
+                    </h2>
+                    <div id="collapseEightA" class="accordion-collapse collapse" aria-labelledby="headingEightA" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Yes, governments and municipalities all around the world are implementing VASDs because of their beneficial effects on traffic safety. Their usage continues to expand as more regions recognize their effectiveness.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingEight">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                            9.	Can VASDs be used in conjunction with other traffic management systems?
+                        </button>
+                    </h2>
+                    <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: Yes, VASDs can be integrated with other systems like traffic cameras, variable message signs, and intelligent transportation systems to create a comprehensive approach to traffic management and safety.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="headingNine">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                            10.	Are there any potential drawbacks to VASDs?
+                        </button>
+                    </h2>
+                    <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            Answer: While VASDs are typically advantageous, there are certain issues, such as driver overdependence on the displays and the need for ongoing maintenance to provide accurate data and visibility.
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+
 <!-- _____________________ourclint-last-start___________________ -->
 @include('customer.layout2.our_clients')
 <!-- _____________________ourclint-last-end___________________ -->
