009681c59d2d47790f4e5bfa62475aa791b94412
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-09-27 12:25:39 +0530

    homepage font url chnage

--- resources/views/customer/layout2/header.blade.php
+++ resources/views/customer/layout2/header.blade.php
@@ -36,7 +36,7 @@
 {{--        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"--}}
 {{--        rel="stylesheet">--}}
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" as="style">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+
 
     <link rel="stylesheet" type="text/css" href="/assets/customer/slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="/assets/customer/slick/slick-theme.css"/>
