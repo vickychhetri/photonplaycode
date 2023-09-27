61b6e59d83d4a3e208f0bcca096008cf301250cb
Hence forth <vicky.chhetri.henceforth@gmail.com>
2023-09-27 12:32:29 +0530

    homepage font url chnage

--- resources/views/customer/layout2/header.blade.php
+++ resources/views/customer/layout2/header.blade.php
@@ -32,11 +32,16 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-{{--    <link--}}
-{{--        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"--}}
-{{--        rel="stylesheet">--}}
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" as="style">
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" media="print" onload="this.media='all'" as="style" onload="this.media='all'">
+    <style>
+        /* Define a local fallback font */
+        @font-face {
+            font-family: 'Poppins';
+            font-weight: normal;
+            font-style: normal;
+            src: local('Arial'), local('sans-serif');
+        }
+    </style>
 
     <link rel="stylesheet" type="text/css" href="/assets/customer/slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="/assets/customer/slick/slick-theme.css"/>
