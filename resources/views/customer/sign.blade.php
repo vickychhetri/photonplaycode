71c0b5de6c7ffac8a892fdf7fe0898080e869c08
Vicky Chhetri <specialpower321@gmail.com>
2024-01-09 23:33:44 +0530

    funtionality paragraph devide into two

--- resources/views/customer/sign.blade.php
+++ resources/views/customer/sign.blade.php
@@ -310,7 +310,7 @@ $seo_meta = [
     <div class="heading-sec">
         <h2 class="fs-md-2 mt-3">Radar Speed Signs Multi Functional Display</h2>
         <p>Acts like a Messenger and a Speed Sign Full matrix-based Digital Speed Limit Sign with multi-functional
-            display in 4 colors with high bright in-built warning FLASHING LIGHTS.</p>
+            <br/>display in 4 colors with high bright in-built warning FLASHING LIGHTS.</p>
     </div>
     <div class="container ">
         <div class="accodion-wrapper pb-5">
