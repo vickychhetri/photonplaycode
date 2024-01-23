93ba53e485d8967505f081ef825a88a92c73074b
Vicky Chhetri <specialpower321@gmail.com>
2024-01-24 00:01:49 +0530

    PPS-2 Meta tag in Radar product page

--- resources/views/components/customer/meta-seo-tag.blade.php
+++ resources/views/components/customer/meta-seo-tag.blade.php
@@ -1,19 +1,19 @@
 @if(isset($seodata['title']))
-<title> {{$seodata['title']}} </title>
-<meta name="title" content="{{$seodata['title']}}">
+    <title> {{$seodata['title']}} </title>
+    <meta name="title" content="{{$seodata['title']}}">
 @endif
 @if(isset($seodata['description']))
     <meta name="description" content="{{$seodata['description']}}">
 @endif
-<meta name="google-site-verification" content="x2TVSaiGBx9F_unjNk_O1mEB64-JF5s3lmTguSQvstw" />
+<meta name="google-site-verification" content="x2TVSaiGBx9F_unjNk_O1mEB64-JF5s3lmTguSQvstw"/>
 
-<link rel = "icon" href =
+<link rel="icon" href=
     "{{asset('assets/images/photon_small.png')}}"
-      type = "image/x-icon">
-<link rel="canonical" href="{{Illuminate\Support\Facades\URL::current()}}" />
+      type="image/x-icon">
+<link rel="canonical" href="{{Illuminate\Support\Facades\URL::current()}}"/>
 
 @if(isset($seodata['keywords']))
-<meta name="keywords" content="{{$seodata['keywords']}}">
+    <meta name="keywords" content="{{$seodata['keywords']}}">
 @endif
 
 <!-- Open Graph / Facebook -->
@@ -33,9 +33,9 @@
 <!-- Static -->
 <meta name="author" content="Photonplay">
 <meta name="publisher" content="Photonplay">
-<meta property="locale" content="en_US" />
-<meta property="type" content="website" />
-<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+<meta property="locale" content="en_US"/>
+<meta property="type" content="website"/>
+<meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/>
 
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -43,9 +43,11 @@
 <meta name="revisit-after" content="7 days">
 <meta name="author" content="Photonplay">
 
- 
+
 <meta property="og:image" content="{{asset($seodata['feature_image']??'assets\images\photonshare_image.png')}}">
 <meta property="twitter:image" content="{{asset($seodata['feature_image']??'assets\images\photonshare_image.png')}}">
+
+@stack('header_meta_content')
 {{--Schema--}}
 {!! $seodata['schema']??'' !!}
 {{--Schema End--}}
