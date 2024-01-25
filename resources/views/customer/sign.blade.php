158e2518a2c266cc8aef798b73ed95f2d25194ac
Vicky Chhetri <specialpower321@gmail.com>
2024-01-25 08:16:49 +0530

    PPS-11: Increase the image size

--- resources/views/customer/sign.blade.php
+++ resources/views/customer/sign.blade.php
@@ -114,7 +114,7 @@ $seo_meta = [
                         <div class="p-2">
                             <div class="product_highlight inner-product bg-white">
                                 <div class=" w-75 h-75 light-product m-auto"
-                                     style="background: url('{{ asset('storage/'. $product->cover_image) }}') no-repeat center; background-size: contain;">
+                                     style="background: url('{{ asset('storage/'. $product->cover_image) }}') no-repeat center; background-size: cover;">
                                     {{--                                    <img class=""  src="" alt="">--}}
                                 </div>
                                 <div class="speed-sign text-center mt-3">
