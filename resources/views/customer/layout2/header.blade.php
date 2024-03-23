32d028531283f79f8182c5dec688b25cb51ab3f2
Vicky Chhetri <specialpower321@gmail.com>
2024-03-23 13:35:31 +0530

    preconnect  remove

--- resources/views/customer/layout2/header.blade.php
+++ resources/views/customer/layout2/header.blade.php
@@ -23,7 +23,7 @@
     <x-Customer.MetaSeoTag :seodata="$seo_meta??0"/>
     {{--        End    Start meta--}}
 
-    <script rel="preconnect" src ="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src ="https://www.google.com/recaptcha/api.js" async defer></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
