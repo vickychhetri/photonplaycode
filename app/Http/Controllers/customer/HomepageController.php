5288e970610f43aba60239a9aa3cf53c44ee789d
Vicky Chhetri <specialpower321@gmail.com>
2024-03-25 23:25:57 +0530

    homepage cache

--- app/Http/Controllers/customer/HomepageController.php
+++ app/Http/Controllers/customer/HomepageController.php
@@ -11,14 +11,25 @@ use App\Models\TeamMember;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 
 class HomepageController extends Controller
 {
-
     public function home_page(){
-        $products = Product::with('category')->take(1)->get();
-        $postsSlice = Http::get(env('WORDPRESS_BASE_URL') . 'wp-json/wp/v2/posts?_embed=1&orderby=date&order=desc')->json();
-        $blogs = array_slice($postsSlice, 0 , 3);
-        return view('customer.homepage', compact('products','blogs'));
+        $cacheKey = 'homepage_data';
+        if (Cache::has($cacheKey)) {
+            $cachedData = Cache::get($cacheKey);
+            $products = $cachedData['products'];
+            $blogs = $cachedData['blogs'];
+        } else {
+            $products = Product::with('category')->take(1)->get();
+            $postsSlice = Http::get(env('WORDPRESS_BASE_URL') . 'wp-json/wp/v2/posts?_embed=1&orderby=date&order=desc')->json();
+            $blogs = array_slice($postsSlice, 0 , 3);
+            $dataToCache = ['products' => $products, 'blogs' => $blogs];
+            Cache::put($cacheKey, $dataToCache, now()->addMinutes(600));
+        }
+
+        // Return the view with the data
+        return view('customer.homepage', compact('products', 'blogs'));
     }
 }
