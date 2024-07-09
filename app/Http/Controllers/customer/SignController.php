203ef15c5f2cb67a5fc701c0180c48d44a9745e9
Vicky Chhetri <specialpower321@gmail.com>
2024-07-09 22:55:44 +0530

    vendor page store

--- app/Http/Controllers/customer/SignController.php
+++ app/Http/Controllers/customer/SignController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Psy\VersionUpdater\Downloader;
 
 class SignController extends Controller
