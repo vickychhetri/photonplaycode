2fc756008a64d33622f57dea1171967bf9f6a4aa
Vicky Chhetri <specialpower321@gmail.com>
2024-11-16 00:15:49 +0530

    remove price revised

--- resources/views/product/edit-product.blade.php
+++ resources/views/product/edit-product.blade.php
@@ -8,10 +8,48 @@
 
 @section('style')
     <style>
-        input:checked + span {
-            background-color: green;
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 50px;
+            height: 25px;
         }
-        input:checked + span > span {
+
+        .switch input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            transition: background-color 0.4s;
+            border-radius: 34px;
+        }
+
+        .slider::before {
+            content: '';
+            position: absolute;
+            height: 15px;
+            width: 15px;
+            border-radius: 50%;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            transition: transform 0.4s;
+        }
+
+        input:checked + .slider {
+            background-color: #4caf50;
+        }
+
+        input:checked + .slider::before {
             transform: translateX(25px);
         }
     </style>
@@ -183,16 +221,10 @@
                                         <div class="row mb-2 form-group">
                                             <label for="price" class="col-md-2 col-form-label text-md-end">{{ __('Hide Price') }}</label>
                                             <div class="col-md-10 mt-1">
-                                                <label style="position: relative; display: inline-block; width: 50px; height: 25px;">
+                                                <label class="switch">
                                                     <input name="is_price_hide" type="checkbox"
-                                                           style="opacity: 0; width: 0; height: 0;"
                                                            @if($product->is_price_hide == 1) checked @endif>
-                                                    <span style="position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0;
-                                                               background-color: #ccc; transition: 0.4s; border-radius: 34px;">
-                                                    <span style="position: absolute; content: ''; height: 15px; width: 15px;
-                                                                border-radius: 50%; left: 4px; bottom: 4px; background-color: white;
-                                                                transition: 0.4s;"></span>
-                                                  </span>
+                                                    <span class="slider"></span>
                                                 </label>
                                             </div>
                                         </div>
