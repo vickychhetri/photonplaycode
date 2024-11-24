c060459e3fcd781ffdaee07fdc0007fc63bb21ab
Vicky Chhetri <specialpower321@gmail.com>
2024-11-24 13:00:59 +0530

    Web dealer subscriptioKn model

---
+++ app/Http/Controllers/DealerSubscriptionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DealerSubscription;
+use Illuminate\Http\Request;
+
+class DealerSubscriptionController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:dealer_subscriptions,email',
+        ]);
+
+        $ipAddress = $request->ip();
+
+        // Store the subscription with the email and IP address
+        DealerSubscription::create([
+            'email' => $request->email,
+            'ip_address' => $ipAddress,  // Capture IP address server-side
+            'records_status' => 1, // Active by default
+        ]);
+        return redirect()->route('customer.radar.speed.signs_v1')->with('success', 'Subscription added successfully.')->withFragment('dealer-subscribe');;
+
+
+    }
+}
