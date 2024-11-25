c74d4807131ad37e36bdea28285444d367b136ca
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2024-11-25 12:14:07 +0530

    Update sign.blade.php

--- resources/views/signv1/sign.blade.php
+++ resources/views/signv1/sign.blade.php
@@ -186,6 +186,10 @@
                             PRODUCT
                         </a>
                         <ul class="dropdown-menu rounded-3 shadow-lg p-2" aria-labelledby="productDropdown">
+
+                           <li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="/radar-cloud-management"> CLOUD SOFTWARE  </a></li>
+     
+
                             <li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1200">iCop R1200</a></li>
                             <li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1500">iCop R1500</a></li>
                             <li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1500m">iCop R1500M</a></li>
@@ -196,10 +200,6 @@
                         <a class="nav-link" href="/blogs">NEWS & MEDIA</a>
                     </li>
 
- <li class="nav-item">
-                        <a class="nav-link" href="/radar-cloud-management"> CLOUD SOFTWARE 
-                            </a>
-                    </li>
 
 
                     <li class="nav-item">
