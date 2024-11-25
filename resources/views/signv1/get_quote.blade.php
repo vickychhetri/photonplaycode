bd343b27c739e14631ebe7e610e0d9803b90eb18
Vicky Chhetri <specialpower321@gmail.com>
2024-11-25 06:39:55 +0530

    v1 launched dealer active menu

--- resources/views/signv1/get_quote.blade.php
+++ resources/views/signv1/get_quote.blade.php
@@ -159,7 +159,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active text-dark" href="{{route('customer.radar.speed.signs')}}">HOME</a>
+                        <a class="nav-link text-dark" href="{{route('customer.radar.speed.signs')}}">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-dark" href="/company">COMPANY</a>
@@ -179,7 +179,7 @@
                         <a class="nav-link text-dark" href="/blogs">NEWS & MEDIA</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-dark" href="{{route('customer.radar.speed.signs__get_quote_v1')}}" >GET QUOTE</a>
+                        <a class="nav-link active text-dark" href="{{route('customer.radar.speed.signs__get_quote_v1')}}" >GET QUOTE</a>
                     </li>
                 </ul>
 
