864e3bb29f23571638b34d9843a6e3601ade962e
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2024-11-25 12:32:56 +0530

    Update get_quote.blade.php

--- resources/views/signv1/get_quote.blade.php
+++ resources/views/signv1/get_quote.blade.php
@@ -170,11 +170,13 @@
                         </a>
                         <ul class="dropdown-menu rounded-3 shadow-lg p-2" aria-labelledby="productDropdown">
 
-     <li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="/radar-cloud-management"> CLOUD SOFTWARE  </a></li>
+     
                             <li><a class="dropdown-item px-4 py-3 text-dark border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1200">iCop R1200</a></li>
                             <li><a class="dropdown-item px-4 py-3 text-dark border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1500">iCop R1500</a></li>
                             <li><a class="dropdown-item px-4 py-3 text-dark border-bottom hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1500m">iCop R1500M</a></li>
                             <li><a class="dropdown-item px-4 py-3 text-dark hover-bg-light" href="https://www.photonplay.com/radar-speed-signs/model/r1800m">iCop R1800M</a></li>
+
+<li><a class="dropdown-item px-4 py-3 text-black border-bottom hover-bg-light" href="/radar-cloud-management"> Cloud Software  </a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
