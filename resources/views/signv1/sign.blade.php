24477966214b92fd5487b8a806d050dc4fd2a0c9
Vicky Chhetri <specialpower321@gmail.com>
2024-11-25 07:27:56 +0530

    chat

--- resources/views/signv1/sign.blade.php
+++ resources/views/signv1/sign.blade.php
@@ -1553,6 +1553,17 @@
     })
 </script>
 
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5da70a23df22d913399f714f/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
 
 <script>
     $(document).ready(function () {
