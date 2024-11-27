d42034932d3987411829ef38bac9afc01430d85e
Vicky Chhetri <82648574+vickychhetri@users.noreply.github.com>
2024-11-27 16:32:51 +0530

    Update sign.blade.php

--- resources/views/signv1/sign.blade.php
+++ resources/views/signv1/sign.blade.php
@@ -154,10 +154,12 @@
             height: auto;
         }
     </style>
-
+   <!-- Google Tag Manager -->     <script>(function (w, d, s, l, i) {             w[l] = w[l] || []; w[l].push({                 'gtm.start':                     new Date().getTime(), event: 'gtm.js'             }); var f = d.getElementsByTagName(s)[0],                 j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =                 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);         })(window, document, 'script', 'dataLayer', 'GTM-NJZBCGR');</script>     <!-- End Google Tag Manager -->
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) --> <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJZBCGR" height="0" width="0"                   style="display:none;visibility:hidden"></iframe></noscript> <!-- End Google Tag Manager (noscript) -->
+
 <a href="/dealership" class="rotated-button" style="text-decoration: none;color: white;">Want to be Dealers/Partners?</a>
 <!-- ------------------ Hero section------------------ -->
 <section class="banner-section pt-0">
