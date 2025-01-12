000ab4f59e1511d73c68625217834bee2fbf7fdd
Vicky Chhetri <specialpower321@gmail.com>
2025-01-12 17:50:55 +0530

    main updtaed

--- resources/views/livewire/radar.blade.php
+++ resources/views/livewire/radar.blade.php
@@ -141,7 +141,7 @@
                                                             data-code="{{$specilization->specilization->code}}"
                                                             wire:ignore
                                                             required>
-                                                        <option value="null" disabled selected>--Choose an Option--</option>
+                                                        <option value="null"  selected>--Choose an Option--</option>
                                                         <!-- Loop through each option for this specialization -->
                                                         @foreach($specilization->options as $option)
                                                             <option value="{{ $option->id }}"  data-code="{{ $option->specializationoptions->code }}">
@@ -251,13 +251,40 @@
                                                         return specializationCode + (optionCode ? '/' + optionCode : ''); // Combine specialization and option codes with '/'
                                                     }).filter(Boolean);
 
-                                                    // Combine product code and other SKU parts with '-' between different specifications
-                                                    const sku = productCode + '-' + skuParts.join('-');
+                                                    const sku =skuParts.join('-');
+                                                    // Send AJAX request
+                                                    $.ajax({
+                                                        url: "{{ route('radar_speed_sign.find_sku',$product->id) }}", // The route we defined earlier
+                                                        type: 'POST',
+                                                        data: {
+                                                            product_id: {{$product->id}},
+                                                            sku: sku,
+                                                            _token: '{{ csrf_token() }}' // CSRF token for security
+                                                        },
+                                                        success: function(response) {
+                                                            // Handle success
+                                                            if (response.status === 'success') {
+                                                                if (response && response.data && Object.keys(response.data).length !== 0) {
+                                                                    var sku_formed='PSI-'+'{{$product->code}}' +'-'+ response.data;
+                                                                    skuInput.value =sku_formed;
+                                                                    skuDisplay.textContent = sku_formed;
+                                                                @this.set('product_sku_code', sku_formed);
+                                                                }else {
+                                                                    skuInput.value =null;
+                                                                    skuDisplay.textContent = null;
+                                                                @this.set('product_sku_code', null);
+                                                                }
+                                                            } else {
+                                                                $('#result').html('<p>' + response.message + '</p>');
+                                                            }
+                                                        },
+                                                        error: function(xhr, status, error) {
+                                                            skuInput.value =null;
+                                                            skuDisplay.textContent = null;
+                                                        @this.set('product_sku_code', null);
+                                                        }
+                                                    });
 
-                                                    // Update the hidden input value and the display span
-                                                    skuInput.value = sku;
-                                                    skuDisplay.textContent = sku;
-                                                   @this.set('product_sku_code', sku);
                                                 }
 
                                                 selects.forEach(select => {
