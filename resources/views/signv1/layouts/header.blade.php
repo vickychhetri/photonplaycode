ed8f09977f2f487e40c8ba2ac232457e7e824988
Vicky Chhetri <specialpower321@gmail.com>
2025-01-22 08:16:34 +0530

    css  remove carousel

--- resources/views/signv1/layouts/header.blade.php
+++ resources/views/signv1/layouts/header.blade.php
@@ -98,8 +98,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/signv1/assets/styles/index.css">
-    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+{{--    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">--}}
+{{--    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">--}}
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
 
