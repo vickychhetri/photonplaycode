35db28e0a0f1e32ecf96e82496c1c4b8a9871c75
Vicky Chhetri <specialpower321@gmail.com>
2025-01-23 23:04:29 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -422,3 +422,41 @@ Route::group(['prefix' => 'radar_speed_sign', 'as' => 'radar_speed_sign.'], func
 
 
 require_once base_path('routes/policy.php');
+
+
+
+/**
+ * Route for displaying the Warranty and Guarantee Policy page.
+ */
+Route::get('warranty-guarantee', [RadarPolicyController::class, 'warranty_guarantee'])->name('radar.warranty_guarantee');
+
+/**
+ * Route for displaying the Security and Privacy Policy page.
+ */
+Route::get('security-privacy-policy', [RadarPolicyController::class, 'security_privacy_policy'])->name('radar.security_privacy_policy');
+
+/**
+ * Route for displaying the Secure Shopping Policy page.
+ */
+Route::get('secure-shopping', [RadarPolicyController::class, 'secure_shopping'])->name('radar.secure_shopping');
+
+/**
+ * Route for displaying the Do Not Sell or Share Policy page.
+ */
+Route::get('do-not-sell', [RadarPolicyController::class, 'do_not_sell'])->name('radar.do_not_sell');
+
+/**
+ * Route for displaying the Shipping and Return Policy page.
+ */
+Route::get('shipping-return', [RadarPolicyController::class, 'shipping_return'])->name('radar.shipping_return');
+
+/**
+ * Route for displaying the Terms and Conditions page.
+ */
+Route::get('photon-terms-conditions', [RadarPolicyController::class, 'terms_conditions'])->name('radar.terms_conditions');
+
+/**
+ * Route for displaying the Accessibility Statement page.
+ */
+Route::get('accessibility-statement', [RadarPolicyController::class, 'accessibility_statement'])->name('radar.accessibility_statement');
+
