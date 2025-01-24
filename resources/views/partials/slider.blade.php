6f28992546c0e73f8588406186d6da6b8cc7fe14
Vicky Chhetri <specialpower321@gmail.com>
2025-01-24 21:03:48 +0530

    rider

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -28,9 +28,9 @@
 <div class="radar-scroll-container" wire:ignore>
     @foreach($product->images as $im_g)
         <div>
-            <div class="radar-item-box" style="">
+            <div class="radar-item-box" style="" wire:ignore>
                 <img src="{{ asset('storage/thumbnail/' . $im_g->image) }}" class="img-fluid"
-                     alt="{{ $product->title }}">
+                     alt="{{ $product->title }}" wire:ignore>
             </div>
         </div>
     @endforeach
